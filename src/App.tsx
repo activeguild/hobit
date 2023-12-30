6f2fe6d59206e31482bb3514d110613792bf2e34
j1ngzoue <workdrive0306@gmail.com>
2023-12-31 00:20:55 +0900

    Add @yamada-ui/react and @yamada-ui/table dependencies, update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { UIProvider } from "@yamada-ui/react";
+import { FileInput } from "@yamada-ui/file-input";
+import { Column, Table, RowData } from "@yamada-ui/table";
+import { useMemo } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const columns = useMemo<Column<RowData>[]>(
+    () => [
+      {
+        header: "作品名",
+        accessorKey: "name",
+      },
+      {
+        header: "放送期間",
+        accessorKey: "broadcastPeriod",
+      },
+      {
+        header: "話数",
+        accessorKey: "episode",
+      },
+    ],
+    []
+  );
+
+  const data = useMemo<RowData[]>(
+    () => [
+      {
+        name: "ドラゴンボール",
+        broadcastPeriod: "1986年2月26日 - 1989年4月19日",
+        episode: "全153話",
+      },
+      {
+        name: "ドラゴンボールZ",
+        broadcastPeriod: "1989年4月26日 - 1996年1月31日",
+        episode: "全291話 + スペシャル2話",
+      },
+      {
+        name: "ドラゴンボールGT",
+        broadcastPeriod: "1996年2月7日 - 1997年11月19日",
+        episode: "全64話 + 番外編1話",
+      },
+      {
+        name: "ドラゴンボール改",
+        broadcastPeriod: "2009年4月5日 - 2015年6月28日",
+        episode: "全159話",
+      },
+      {
+        name: "ドラゴンボール超",
+        broadcastPeriod: "2015年7月5日 - 2018年3月25日",
+        episode: "全131話",
+      },
+    ],
+    []
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <UIProvider>
+      <FileInput placeholder={"Please select an image"} />
+      <Table columns={columns} data={data} />
+    </UIProvider>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vpc from "vite-plugin-cloudflare";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vpc({ scriptPath: "./functions/example.ts" })],
+  plugins: [react()],
 });
