1cf8d1f67f3a81c4c07e2ccad8900e03c301967b
j1ngzoue <workdrive0306@gmail.com>
2024-01-02 21:32:14 +0900

    debug

--- functions/translate.ts
+++ functions/translate.ts
@@ -11,29 +11,30 @@ export const onRequestOptions = async () => {
 };
 
 export const onRequestPost = async (content) => {
-  console.log("content :>> ", content);
+  // console.log("content :>> ", content);
   const requestText = await content.request.text();
   const requestParams = JSON.parse(requestText);
-  const subDomain = requestParams.free_api ? "api-free" : "api";
+  console.log("requestParams :>> ", requestParams);
+  // const subDomain = requestParams.free_api ? "api-free" : "api";
 
-  const deeplResponse = await fetch(
-    `https://${subDomain}.deepl.com/v2/translate`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `DeepL-Auth-Key ${requestParams.auth_key}`,
-        "Access-Control-Allow-Origin": "*",
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        text: requestParams.texts,
-        target_lang: requestParams.target_lang,
-      }),
-    }
-  );
+  // const deeplResponse = await fetch(
+  //   `https://${subDomain}.deepl.com/v2/translate`,
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: `DeepL-Auth-Key ${requestParams.auth_key}`,
+  //       "Access-Control-Allow-Origin": "*",
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       text: requestParams.texts,
+  //       target_lang: requestParams.target_lang,
+  //     }),
+  //   }
+  // );
 
-  const deeplJson = await deeplResponse.json();
-  return new Response(JSON.stringify(deeplJson), {
+  // const deeplJson = await deeplResponse.json();
+  return new Response(JSON.stringify({}), {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "*",
